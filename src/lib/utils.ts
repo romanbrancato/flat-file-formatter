001de898ebcaff0a567f523973695882f88b1039
romanbrancato <romanbrancato88@gmail.com>
2025-02-13 15:22:57 -0500

    cleaned up new toolbar

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function download(content: Uint8Array, filename: string) {
+  // Create a Blob from the Uint8Array
+  const blob = new Blob([content], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  setTimeout(() => URL.revokeObjectURL(url), 60_000);
 }
